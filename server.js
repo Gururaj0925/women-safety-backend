12261b07091a99166e969d4fc0b84cc50371ca54
Gururaj0925 <g7892712433@gmail.com>
2026-06-29 15:06:43 +0530

    Added MongoDB debug logs

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const crypto = require('crypto');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -403,15 +403,17 @@ const notifyEmergencyContact = async ({ contact, userId, reason, message, locati
 };
 
 // MongoDB Connection
+
+
 console.log("Connecting to MongoDB...");
-console.log("MONGO URI =", process.env.MONGO_URI);
+console.log("MONGO URI RAW:");
+console.log(JSON.stringify(process.env.MONGO_URI));
+
 mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 5000,
-  ssl: true,
-  tls: true
+  serverSelectionTimeoutMS: 30000,
 })
-.then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.error('MongoDB connection error:', err));
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 // User Model
 const userSchema = new mongoose.Schema({
@@ -731,6 +733,5 @@ app.post('/send-sos', async (req, res) => {
 
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
